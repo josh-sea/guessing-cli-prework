95af561338b7e6df75b0c4fc6a9bb54414f86c23
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-18 19:11:29 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,13 +9,10 @@ def exit_call
   puts "Goodbye!"
 end
 
-def user_input
-    gets.chomp
-end
 def run_guessing_game
   puts "Guess a number between 1 and 6."  
   number = rand(1..6).to_s
-  user_input
+  user_input = gets.chomp
   if user_input == number
     puts "You guessed the correct number!"
     exit_call
